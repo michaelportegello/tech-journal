28359640cd54ffecd97ed4e119ae0cb119511eb3
michaelportegello <70924561+michaelportegello@users.noreply.github.com>
2021-03-10 18:07:08 -0500

    Update secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,9 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+echo sudo useradd -m -d /home/sys265 -s /bin/bash sys265
+echo sudo mkdir /home/sys265/.ssh
+echo sudo cp SYS265/linux/public-keys/key.pub /home/sys265/.ssh/authorized_keys
+echo sudo chmod 700 /home/sys265/.ssh
+echo sudo chmod 600 /home/sys265/.ssh/authorized_keys
+echo sudo chown -R sys265:sys265 /home/sys265/.ssh
